f900e2ce40f5ac7861951a06070bbfb7361ea014
Tharun-119 <tharunkesireddy119@gmail.com>
2024-07-24 14:04:41 +0530

    Update 24 - 07 - 24.py

--- 24/24 - 07 - 24.py
+++ 24/24 - 07 - 24.py
@@ -124,3 +124,19 @@ sum of all numbers in array using recurison
 #         temp+=i
 #     return temp
 # print(recursive_add(arr1))
+
+
+# Palindrome
+a1 = "racear"
+def palindrome(a):
+    i = 0
+    j = len(a) - 1
+    while i <= j:
+        if a[i] == a[j]:
+            i += 1
+            j -= 1
+        else:
+            return False
+    return True
+
+print(palindrome(a1))
